1e6ae8f5a037de722a23e39cec8a78dd9932392c
Ankiiisharma <myselfankit.2003@gmail.com>
2023-05-20 23:54:01 +0530

    Roman to Integer- failing 1 test case yet

---
+++ Romantointeger.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int temp=0;
+        char string[10];
+        scanf("%s",&string);
+
+        for(int i=0;i<strlen(string);i++)
+        {
+            if(string[i]=='I')
+            {
+                temp=temp+1;
+            }
+            else if(string[i]=='V')
+            {
+                temp=temp+5;
+            }
+            else if(string[i]=='X')
+            {
+                temp=temp+10; 
+            }
+            else if(string[i]=='L')
+            {
+                temp=temp+50;
+            }
+            else if(string[i]=='C')
+            {
+                temp=temp+100;
+            }
+            else if(string[i]=='V')
+            {
+                temp=temp+500;
+            }
+            else if(string[i]=='V')
+            {
+               temp=temp+1000; 
+            }
+        }
+        printf("%d",temp);
+    }
+}
