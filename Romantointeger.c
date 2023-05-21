6aec43a641454e78fa4cb1fa1f6c505e5839ee98
Ankiiisharma <myselfankit.2003@gmail.com>
2023-05-21 16:01:34 +0530

    updated

--- Romantointeger.c
+++ Romantointeger.c
@@ -13,6 +13,8 @@ void main()
 
         for(int i=0;i<strlen(string);i++)
         {
+       if(string[i]<string[i+1])
+        {      
             if(string[i]=='I')
             {
                 temp=temp+1;
@@ -41,6 +43,39 @@ void main()
             {
                temp=temp+1000; 
             }
+        }
+        else
+        {
+            if(string[i]=='I')
+            {
+                temp=temp-1;
+            }
+            else if(string[i]=='V')
+            {
+                temp=temp-5;
+            }
+            else if(string[i]=='X')
+            {
+                temp=temp-10; 
+            }
+            else if(string[i]=='L')
+            {
+                temp=temp-50;
+            }
+            else if(string[i]=='C')
+            {
+                temp=temp-100;
+            }
+            else if(string[i]=='V')
+            {
+                temp=temp-500;
+            }
+            else if(string[i]=='V')
+            {
+               temp=temp-1000; 
+            }
+        }
+
         }
         printf("%d",temp);
     }
