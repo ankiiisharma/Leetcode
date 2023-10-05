34a548198a8391b03282b613e3819a0d47275b2e
Ankiiisharma <myselfankit.2003@gmail.com>
2023-10-05 23:54:29 +0530

    DAY 5

--- 88leetcode.cpp
+++ 88leetcode.cpp
@@ -1,26 +1,72 @@
-class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int i=m-1;
+//         int j=n-1;
+//         int k=m+n-1;
 
-        while(i>=0 && j>=0){
+//         while(i>=0 && j>=0){
+//             if(nums1[i]>nums2[j]){
+//                 nums1[k--]=nums1[i--];
+//                 // i--;
+//             }
+//             else{
+//                 nums1[k--]=nums2[j--];
+//                 // j--;
+//             }
+//             // k--;
+//         }
+
+//         while(j>=0){
+//             nums1[k--]=nums2[j--];
+//             // j--;
+//             // k--;
+//         }
+//     }
+// };
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int>& nums1, int m,vector<int>& nums2, int n){
+    int i=m-1;
+    int j=n-1;
+    int k=m+n-1;
+
+    while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
-                nums1[k--]=nums1[i--];
-                // i--;
+                nums1[k]=nums1[i];
+                i--;
             }
             else{
-                nums1[k--]=nums2[j--];
-                // j--;
+                nums1[k]=nums2[j];
+                j--;
             }
-            // k--;
+            k--;
         }
 
         while(j>=0){
-            nums1[k--]=nums2[j--];
-            // j--;
-            // k--;
+            nums1[k]=nums2[j];
+            j--;
+            k--;
         }
-    }
 };
+
+
+int main()
+{
+    int m=3;
+    int n=3;
+    
+    vector<int>nums1 = {1,2,3,0,0,0};
+    vector<int>nums2 = {2,5,6}
+
+    merge(nums1,m,nums2,n)
+
+    for(auto num : nums1){
+        cout<<num<<" ";
+    }
+   
+    return 0;
+}
