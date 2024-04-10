95b1804a231658ca60d21eb66a7796691f68fd12
Ankiiisharma <myselfankit.2003@gmail.com>
2024-04-10 22:57:54 +0530

    potd 10.04

--- 01exes/medium.cpp
+++ 01exes/medium.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution{
+  public:
+  int MakeMedianEqualToK(vector<int>&nums, int k){
+    int totalOperations = 0;
+    int n = nums.size();
+    sort(nums.begin(),nums.end()); // 1 2 3 4 5 6
+    int median = nums[n/2];
+
+    for(int i=0;i<n;i++){
+        if(median < k){
+            
+        }
+    }
+
+  }  
+};
+
 int main()
 {
     vector<int>nums = {1,2,3,4,5,6};
