1b3d0731b67969963430edb950e7760fb4c97e89
Ankiiisharma <myselfankit.2003@gmail.com>
2024-05-15 06:20:18 +0530

    Score After Flipping Matrix

--- POTD/brute/ScoreAfterFlipping.cpp
+++ POTD/brute/ScoreAfterFlipping.cpp
@@ -1,40 +1,62 @@
-class Solution {
-public:
-    int matrixScore(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+#include<bits/stdc++.h>
+using namespace std;
 
-        for(int i=0;i<m;i++){
-            if(grid[i][0] == 0){
-                for(int j=0;j<n;j++){
-                    grid[i][j] = 1 - grid[i][j];
-                }
-            }
-        }
+int ScoreAfterFlipping(vector<vector<int>>& grid){
+    int m = grid.size();
+    int n = grid[0].size();
 
-        for(int j=1;j<n;j++){
-            int countZero = 0;
-            for(int i=0;i<m;i++){
-                if(grid[i][j]==0){
-                    countZero += 1;
-                }
-            }
-        int countOne = m -countZero;
-            if(countZero > countOne ){
-                for(int i=0;i<m;i++){
-                grid[i][j] = 1 - grid[i][j];
-                }
-          }
-        }
-
-
-        int score = 0;
-        for(int i=0;i<m;i++){
+    for(int i=0;i<m;i++){
+        if(grid[i][0] == 0){
+            //flip
             for(int j=0;j<n;j++){
-                int value = grid[i][j] * pow(2,n-j-1);
-                score += value;
+                grid[i][j] = 1 - grid[i][j];
             }
         }
-        return score;
     }
-};
+
+    for(int j=1;j<n;j++){
+        int countZero = 0;
+        for(int i=0;i<m;i++){
+            if(grid[i][j] == 0){
+                countZero += 1;
+            }
+        }
+
+        int countOne = m - countZero;
+        if(countZero > countOne){
+            for(int i=0;i<m;i++){
+                grid[i][j] = 1 - grid[i][j];
+            }
+        }
+    }
+
+
+    int score = 0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            int value = grid[i][j]*pow(2,n-j-1);
+            score += value;
+        }
+    }
+
+    return score;
+
+}
+
+int main()
+{
+    int m,n;
+    cin>>m,n;
+
+    vector<int>grid(m,vector<int>(n));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>grid[i][j];
+        }
+    }
+
+    int ans = ScoreAfterFlipping(grid);
+    cout<<ans;
+
+    return 0;
+}
